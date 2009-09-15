15e54ac86e2a98d5211ea3490de68dbf906026ef
Jay Callicott <jaykali@42248.no-reply.drupal.org>
2009-09-15 18:58:43 +0000

    document.ready fix

--- jquery_dropdown_jump/js/jquery_dropdown_jump.js
+++ jquery_dropdown_jump/js/jquery_dropdown_jump.js
@@ -1,9 +1,9 @@
 /**
  * This uses some clever logic to find the parent form of the select and submit the button
  */
-Drupal.behaviors.loadJqueryDropdownJump = function(context) {
+$(document).ready(function() {
   
-  $("ul.jquery_dropdown_jump li a", context).click(function(){
+  $("ul.jquery_dropdown_jump li a").click(function(){
     if ($(this).attr('rel') == '') return;//don't jump if no value
     
     //find the parent form of this jump menu
@@ -12,4 +12,4 @@ Drupal.behaviors.loadJqueryDropdownJump = function(context) {
     //submit the form by clicking the submit button 
     parent_form.find("input.form-submit").click();
   });
-}
+});
